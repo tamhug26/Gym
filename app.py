8f7d28981eac358d487c0c5cb062897be1405788
tamhug26 <tamara@huggler.net>
2026-06-01 17:42:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,38 @@ with Übung:
     if Splitday == "Triceps":
         st.selectbox(
         "Übung",
-        ["Dips", "Push down",  ]
-    
-    if Splitday == "Shoulders":
+        ["Dips", "Push down"  ]
+        )
+        st.selectbox(
+            "Machine",
+            ["cable", "Freigewicht", "Maschine"]
+        )
 
+    if Splitday == "Shoulders":
+        st.selectbox(
+            "Übung",
+            ["Lateral raises", "front raises", "Shoulder Press"]
+        )
+        st.selectbox(
+            "Machine",
+            ["cable", "Freigewicht", "Maschine"]
+        )
+
+with set1:
+    Gewicht = st.number_input("Gewicht", 0, 400, 20)
+    Wdh = st.number_input("Wiederholungen", 0, 30, 8)
+with set2:
+    Gewicht = st.number_input("Gewicht", 0, 400, 20)
+    Wdh = st.number_input("Wiederholungen", 0, 30, 8)
+with set3:
+    Gewicht = st.number_input("Gewicht", 0, 400, 20)
+    Wdh = st.number_input("Wiederholungen", 0, 30, 8)
+with set4:
+    Gewicht = st.number_input("Gewicht", 0, 400, 20)
+    Wdh = st.number_input("Wiederholungen", 0, 30, 8)
+with note:
+    st.text_input(
+        "Note",
+        key="Note",
+    )
 
